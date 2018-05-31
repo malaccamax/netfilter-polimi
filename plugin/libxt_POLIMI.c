8a07071d8378a34ab6f4cd527f32f65f4b2d933e
MalaccaMax.k8s <malaccamax@outlook.com>
2018-05-31 17:27:44 +0800

    Reformat plugin

--- plugin/libxt_POLIMI.c
+++ plugin/libxt_POLIMI.c
@@ -1,6 +1,6 @@
 /* Shared library add-on to iptables to add POLIMI target support. 
 Ex. Usage:
-	iptables -t mangle -A INPUT -t POLIMI --findstring badstring --replacestring goodstring
+  iptables -t mangle -A INPUT -t POLIMI --findstring badstring --replacestring goodstring
 */
 
 #include <linux/netfilter/xt_POLIMI.h>
@@ -17,9 +17,9 @@ Ex. Usage:
  */
 static void
 polimi_help(void) {
-	printf("POLIMI target options:\n"
-	       " --findString String to replace\n"
-	       " --replaceString Replacing string\n");
+  printf("POLIMI target options:\n"
+         " --findString String to replace\n"
+         " --replaceString Replacing string\n");
 }
 
 /**
@@ -28,8 +28,8 @@ polimi_help(void) {
 static void polimi_print(const void *ip,
                          const struct xt_entry_target *target,
                          int numeric) {
-	const struct xt_polimi_info *myinfo = (const struct xt_polimi_info *)target->data;
-	printf("\n POLIMI TARGET: Replace '%s' with '%s'",
+  const struct xt_polimi_info *myinfo = (const struct xt_polimi_info *)target->data;
+  printf("\n POLIMI TARGET: Replace '%s' with '%s'",
          myinfo->findString,
          myinfo->replString);
 }
@@ -38,17 +38,17 @@ static void polimi_print(const void *ip,
  * Parameters
  */
 static const struct option polimi_opts[] = {
-	{.name = "findstring", .has_arg = true, .val = 'f'},
-	{.name = "replacestring", .has_arg = true, .val = 'r'},
-	XT_GETOPT_TABLEEND,
+  {.name = "findstring", .has_arg = true, .val = 'f'},
+  {.name = "replacestring", .has_arg = true, .val = 'r'},
+  XT_GETOPT_TABLEEND,
 };
 
 /**
  * Parameters Validation
  */
 static void polimi_check(unsigned int flags) {
-	if (flags != 3) {
-		xtables_error(PARAMETER_PROBLEM,
+  if (flags != 3) {
+    xtables_error(PARAMETER_PROBLEM,
         "You must specify both parameters and there can't be duplicates!");
   }
 }
@@ -57,10 +57,10 @@ static void polimi_check(unsigned int flags) {
  * Parameters Parsing
  */
 static int polimi_parse(int c, char **argv, int invert, unsigned int *flags,
-		                    const void *entry, struct xt_entry_target **target) {
-	struct xt_polimi_info *myinfo = (struct xt_polimi_info *)(*target)->data;
+                        const void *entry, struct xt_entry_target **target) {
+  struct xt_polimi_info *myinfo = (struct xt_polimi_info *)(*target)->data;
 
-	switch (c) {
+  switch (c) {
     case 'f': { /* --findString <string>*/
       if(strlen(optarg) > POLIMI_TARGET_MAX_STRING_SIZE) {
         xtables_error(PARAMETER_PROBLEM, "findstring too long");
@@ -84,24 +84,24 @@ static int polimi_parse(int c, char **argv, int invert, unsigned int *flags,
     }
 
     default: {}
-	}
+  }
 
-	return 1;
+  return 1;
 }
 
 static struct xtables_target polimi_target = {
-	.name          = "POLIMI",
-	.version       = XTABLES_VERSION,
-	.family        = NFPROTO_IPV4,
-	.size          = XT_ALIGN(sizeof(struct xt_polimi_info)),
-	.userspacesize = XT_ALIGN(sizeof(struct xt_polimi_info)),
-	.help          = polimi_help,
-	.final_check   = polimi_check,
-	.print         = polimi_print,
-	.parse         = polimi_parse,
-	.extra_opts    = polimi_opts,
+  .name          = "POLIMI",
+  .version       = XTABLES_VERSION,
+  .family        = NFPROTO_IPV4,
+  .size          = XT_ALIGN(sizeof(struct xt_polimi_info)),
+  .userspacesize = XT_ALIGN(sizeof(struct xt_polimi_info)),
+  .help          = polimi_help,
+  .final_check   = polimi_check,
+  .print         = polimi_print,
+  .parse         = polimi_parse,
+  .extra_opts    = polimi_opts,
 };
 
 void _init(void) {
-	xtables_register_target(&polimi_target);
+  xtables_register_target(&polimi_target);
 }
